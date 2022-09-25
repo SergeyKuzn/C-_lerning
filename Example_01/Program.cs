df1dfd09e07f995e8aa50a3d4a1c60caaadd78b0
Sergey <sergey-407@yandex.ru>
2022-09-25 16:02:36 +0300

    Внес изменения в Ex_01. Задача №4. Условный оператор If.

--- Example_01/Program.cs
+++ Example_01/Program.cs
@@ -8,14 +8,27 @@
 
 //////////////////////////////////////////////////////////////
 // Задача №2. Нахождение суммы + генератор случайных чисел
-int numberA = new Random().Next(1, 10); // генератор случайных чисел от 1 до 9
-int numberB = new Random().Next(1, 10);
-Console.WriteLine(numberA);
-Console.WriteLine(numberB);
-Console.WriteLine(numberA + numberB);
+// int numberA = new Random().Next(1, 10); // генератор случайных чисел от 1 до 9
+// int numberB = new Random().Next(1, 10);
+// Console.WriteLine(numberA);
+// Console.WriteLine(numberB);
+// Console.WriteLine(numberA + numberB);
 
 //////////////////////////////////////////////////////////////
 // Задача №3. Нахождение частного
 // double numberA = 12;
 // double numberB = 5;
 // Console.WriteLine(numberA / numberB);
+
+//////////////////////////////////////////////////////////////
+// Задача №4. Условный оператор If.
+Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша"){
+    Console.WriteLine("Ура, это же МАША!");
+}
+else{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
