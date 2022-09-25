35659712d862046aa2bb75527783223742d21af4
Sergey <sergey-407@yandex.ru>
2022-09-25 19:50:01 +0300

    Внес изменения в Ex_03. Лекция 3. задача 2. замена символов в тексте

--- Example_03/Program.cs
+++ Example_03/Program.cs
@@ -1,16 +1,42 @@
-﻿//Лекция 3. Цикл For. Таблица умножения
+﻿//Лекция 3. 
 
-for (int i = 2; i <= 10; i++)
+
+///////////////////////////////////////////////////////////////////////////////
+// Задача 1. Цикл For. Таблица умножения
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//     {
+//         Console.WriteLine($"{i} x {j} = {i*j}");
+//     }
+//     Console.WriteLine();
+// }
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Задача 2.
+// Дан текст. В тексте нужно все пробелы заменить черточками, 
+// маленькие буквы “к” заменить большими “К”, а большие “С” заменить 
+// маленькими “с”.
+
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что, "
+            + "ежели бы вас послали вместо нашего милого " 
+            + "Винценгероде, вы бы взяли приступом согласие "
+            + "прусского короля. Вы так красноречивы. Вы "
+            + "дадите мне чаю?";
+
+string Replace(string text, char oldValue, char newValue)
 {
-    for (int j = 2; j <= 10; j++)
+    string result = String.Empty;
+    int lenght = text.Length;
+    for (int i = 0; i < lenght; i++)
     {
-        Console.WriteLine($"{i} x {j} = {i*j}");
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
     }
-    Console.WriteLine();
+    return result;
 }
 
-
-
-
-
-
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
