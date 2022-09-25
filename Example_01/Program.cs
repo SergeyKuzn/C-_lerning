2aa311d8f1638962c4883a372084ea1083e2654a
Sergey <sergey-407@yandex.ru>
2022-09-25 15:48:34 +0300

    Внес изменения в Ex_01. Задача №2. Добавили генератор случайных чисел

--- Example_01/Program.cs
+++ Example_01/Program.cs
@@ -7,13 +7,15 @@
 // Console.WriteLine(username);
 
 //////////////////////////////////////////////////////////////
-// Задача №2. Нахождение суммы
-// int numberA = 3;
-// int numberB = 5;
-// Console.WriteLine(numberA + numberB);
+// Задача №2. Нахождение суммы + генератор случайных чисел
+int numberA = new Random().Next(1, 10); // генератор случайных чисел от 1 до 9
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA + numberB);
 
 //////////////////////////////////////////////////////////////
 // Задача №3. Нахождение частного
-double numberA = 12;
-double numberB = 5;
-Console.WriteLine(numberA / numberB);
+// double numberA = 12;
+// double numberB = 5;
+// Console.WriteLine(numberA / numberB);
