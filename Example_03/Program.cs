fd260a7873f803692395ab64e6616b3542d2e7b0
Sergey <sergey-407@yandex.ru>
2022-09-25 19:23:10 +0300

    Внес изменения в Ex_03. Лекция 3. Цикл For. Таблица умножения

--- Example_03/Program.cs
+++ Example_03/Program.cs
@@ -1,11 +1,16 @@
-﻿Console.WriteLine("Лекция №3.");
+﻿//Лекция 3. Цикл For. Таблица умножения
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i*j}");
+    }
+    Console.WriteLine();
+}
 
 
 
 
 
 
-
-
-
-Console.WriteLine("Домашнее задание к семинару №3.");
