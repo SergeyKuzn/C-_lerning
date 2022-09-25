e962ec9f68877c1a40a251878650d59bde5114cb
Sergey <sergey-407@yandex.ru>
2022-09-25 15:33:00 +0300

    Внес изменения в Ex_01. Задача №2. Нахождение суммы

--- Example_01/Program.cs
+++ Example_01/Program.cs
@@ -1,7 +1,19 @@
-﻿// Первый код на C#
+﻿//////////////////////////////////////////////////////////////
+// Задача №1. Первый код на C#
 
+// Console.Write("Введите ваше имя");
+// string username = Console.ReadLine();
+// Console.WriteLine("Hello, ");
+// Console.WriteLine(username);
 
-Console.Write("Введите ваше имя");
-string username = Console.ReadLine();
-Console.WriteLine("Hello, ");
-Console.WriteLine(username);
+//////////////////////////////////////////////////////////////
+// Задача №2. Нахождение суммы
+int numberA = 3;
+int numberB = 5;
+Console.WriteLine(numberA + numberB);
+
+//////////////////////////////////////////////////////////////
+// Задача №3. Нахождение частного
+int numberA = 3;
+int numberB = 5;
+Console.WriteLine(numberA + numberB);
