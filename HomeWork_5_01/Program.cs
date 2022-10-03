519881a722527e9789d17f51c5fc4dc8214a8b3b
Sergey <sergey-407@yandex.ru>
2022-10-03 19:54:22 +0300

    сделал дз № 5_1,2

---
+++ HomeWork_5_01/Program.cs
@@ -0,0 +1,52 @@
+﻿using static System.Console;
+Clear();
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Задача 34: 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая 
+// покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Write("Задайте длину массива: ");
+int arrayLenght = int.Parse(ReadLine()!);
+
+Write("Задайте минимальное значение: ");
+int minValue = int.Parse(ReadLine()!);
+
+Write("Задайте максимальное значение: ");
+int maxValue = int.Parse(ReadLine()!);
+
+int[] array = GetArray(arrayLenght, minValue, maxValue);
+string strArray = string.Join(" | ", array);
+WriteLine($" [{strArray}] ");
+
+int countPozitiveNumber = 0;
+foreach (int element in array)
+{
+    if (element % 2 == 0)
+    {
+        countPozitiveNumber++;
+    }
+}
+WriteLine($"Количество четных чисел в массиве = {countPozitiveNumber}");
+
+
+// Описываем метод
+int[] GetArray(int arrayLenght, int minValue, int maxValue)
+{
+    int[] result = new int[arrayLenght];
+    
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+
+
+
+
+
+
+
---
+++ HomeWork_5_02/Program.cs
@@ -0,0 +1,45 @@
+﻿using static System.Console;
+Clear();
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Задача 36:  
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Write("Задайте длину массива: ");
+int arrayLenght = int.Parse(ReadLine()!);
+
+Write("Задайте минимальное значение: ");
+int minValue = int.Parse(ReadLine()!);
+
+Write("Задайте максимальное значение: ");
+int maxValue = int.Parse(ReadLine()!);
+
+int[] array = GetArray(arrayLenght, minValue, maxValue);
+string strArray = string.Join(" | ", array);
+WriteLine($" [{strArray}] ");
+
+int getSumm = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (i % 2 != 0)
+    {
+        getSumm = getSumm + array[i];
+    }
+}
+WriteLine($"сумму элементов, стоящих на нечётных позициях = {getSumm}");
+
+// Описываем метод
+int[] GetArray(int arrayLenght, int minValue, int maxValue)
+{
+    int[] result = new int[arrayLenght];
+    
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
