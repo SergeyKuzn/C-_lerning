dad53b656a856f489cb691ce2166c353b779abe8
Sergey <sergey-407@yandex.ru>
2022-10-20 11:49:53 +0300

    сделал дз № 9

---
+++ HomeWork_9_02/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+Clear();
+/////////////////////////////////////////////////////////////////////
+
+/*
+    Задача 66: 
+    Задайте значения M и N. Напишите программу, 
+    которая найдёт сумму натуральных элементов 
+    в промежутке от M до N.
+    M = 1; N = 15 -> 120
+    M = 4; N = 8. -> 30
+*/
+
+int a = 4;
+int b = 8;
+
+WriteLine(SummNumber(a, b));
+
+
+
+int SummNumber(int a, int b)
+{
+    if (a == b)
+        return b;
+    return b + SummNumber(a, b - 1);
+}
+
